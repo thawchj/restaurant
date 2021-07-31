68689b9a798d79d027463196891930e31789bdee
unknown <79253649+thawchj@users.noreply.github.com>
2021-07-31 16:24:01 +0700

    update index

--- components/Food.vue
+++ components/Food.vue
@@ -1,133 +1,160 @@
 <template>
-    <div class="my-item">
-        <img :src="image" width="100%" height="auto">
-        <div class="d-flex justify-content-between mt-2">
-            <div class="food-name">{{name}}</div>
-            <div class="food-price">{{price}} บาท</div>
+  <div>
+    <div class="cardimg">
+      <div
+        class="imgbg"
+        :style="{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${image})`,
+        }"
+      ></div>
+      <div class="moutain-in">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-10 col-lg-8 p-md-5 p-4">
+              <div class="texthead-in">{{ name }}</div>
+              <p class="text-in">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Soluta, consectetur dolore rem voluptatibus saepe officiis at
+                cum tenetur itaque alias veritatis
+              </p>
+              <button class="button-order">order now</button>
+            </div>
+          </div>
         </div>
-        <b-form-input v-model="count" type="number" class="my-2" placeholder="ใส่จำนวน"></b-form-input>
-        <b-button  @click="addToCart()" variant="dark" class="w-100">ADD</b-button>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        id: {
-            type: Number,
-            default: 0
-        },
-        name:{
-            type: String,
-            default: ''
-        },
-        price: {
-            type: Number,
-            default: 0
-        },
-        image:{
-            type: String,
-            default: ''
-        }
-
+  props: {
+    id: {
+      type: Number,
+      default: 0,
     },
-    data(){
-        return{
-            count: 0
-        }
+    name: {
+      type: String,
+      default: '',
     },
-    methods: {
-        addToCart(){
-            const item = {
-                id: this.id,
-                name: this.name,
-                price: this.price,
-                count: parseInt(this.count),
-                image: this.image
-            }
-
-            // 1.ดึงค่าจาก คุ๊กกี้ แล้วเช็คว่า มีตัวแปร product-selected หรือยัง
-            const productSelected = this.getCookie('product-selected')
-            if(productSelected === ''){
-                // 1.1 ถ้ายังไม่มี product-selected ให้เพิ่มเข้าไป
-                const itemJSON = JSON.stringify([item])
-                this.setCookie('product-selected',itemJSON,5)
-            }else{
-                // 1.2 ถ้ามี product-selected แล้วให้เช็คค่า ด้านในว่า มีสินค้าที่เราเลือกอยู่ใน product-selectedแล้วหรือยัง
-                const productSelectedArr = JSON.parse(productSelected)
-                const isSelected = productSelectedArr.some(p => p.id === item.id)
-                // 1.2.1 ถ้ายังไม่มีสินค้า ที่เลือก ใน product-selected ให้ push สินค้าเข้าไปได้เลย
-                if (isSelected === false){
-                    productSelectedArr.push(item)
-                    const productSelectedArrJson = JSON.stringify(productSelectedArr)
-                    this.setCookie('product-selected',productSelectedArrJson,5)
-                }
-                // 1.2.2 ถ้ามีสินค้าที่เลือก ใน product-selected ให้ count ของสินค้านั้น เพิ่มตามที่ user กรอกเข้ามา
-                else{
-                    for(let i = 0 ;i < productSelectedArr.length; i++){
-                        if(productSelectedArr[i].id === item.id){
-                            productSelectedArr[i].count += item.count
-                        }
-                    }
-                    const productSelectedArrJson = JSON.stringify(productSelectedArr)
-                    this.setCookie('product-selected',productSelectedArrJson,5)
-                }
-            }
-            
-             
-            
-            
-
-
-
-           
-           
-        },
-        setCookie(cname, cvalue, exdays) {
-        const d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        const expires = "expires="+ d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-        },
-        getCookie(cname) {
-        const name = cname + "=";
-        const decodedCookie = decodeURIComponent(document.cookie);
-        const ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-        } 
+    price: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      count: 0,
     }
+  },
+  methods: {
+    addToCart() {
+      const item = {
+        id: this.id,
+        name: this.name,
+        price: this.price,
+        count: parseInt(this.count),
+        image: this.image,
+      }
+
+      // 1.ดึงค่าจาก คุ๊กกี้ แล้วเช็คว่า มีตัวแปร product-selected หรือยัง
+      const productSelected = this.getCookie('product-selected')
+      if (productSelected === '') {
+        // 1.1 ถ้ายังไม่มี product-selected ให้เพิ่มเข้าไป
+        const itemJSON = JSON.stringify([item])
+        this.setCookie('product-selected', itemJSON, 5)
+      } else {
+        // 1.2 ถ้ามี product-selected แล้วให้เช็คค่า ด้านในว่า มีสินค้าที่เราเลือกอยู่ใน product-selectedแล้วหรือยัง
+        const productSelectedArr = JSON.parse(productSelected)
+        const isSelected = productSelectedArr.some((p) => p.id === item.id)
+        // 1.2.1 ถ้ายังไม่มีสินค้า ที่เลือก ใน product-selected ให้ push สินค้าเข้าไปได้เลย
+        if (isSelected === false) {
+          productSelectedArr.push(item)
+          const productSelectedArrJson = JSON.stringify(productSelectedArr)
+          this.setCookie('product-selected', productSelectedArrJson, 5)
+        }
+        // 1.2.2 ถ้ามีสินค้าที่เลือก ใน product-selected ให้ count ของสินค้านั้น เพิ่มตามที่ user กรอกเข้ามา
+        else {
+          for (let i = 0; i < productSelectedArr.length; i++) {
+            if (productSelectedArr[i].id === item.id) {
+              productSelectedArr[i].count += item.count
+            }
+          }
+          const productSelectedArrJson = JSON.stringify(productSelectedArr)
+          this.setCookie('product-selected', productSelectedArrJson, 5)
+        }
+      }
+    },
+    setCookie(cname, cvalue, exdays) {
+      const d = new Date()
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+      const expires = 'expires=' + d.toUTCString()
+      document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+    },
+    getCookie(cname) {
+      const name = cname + '='
+      const decodedCookie = decodeURIComponent(document.cookie)
+      const ca = decodedCookie.split(';')
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1)
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length)
+        }
+      }
+      return ''
+    },
+  },
 }
 </script>
 
 <style  scoped>
-    .my-item{
-        padding: 10px;
-        background-color: rgb(252, 237, 245);
-        border-radius: 3px;
-        -webkit-box-shadow: 11px 10px 19px -17px rgba(0,0,0,0.75);
-        -moz-box-shadow: 11px 10px 19px -17px rgba(0,0,0,0.75);
-        box-shadow: 11px 10px 19px -17px rgba(0,0,0,0.75);
-    }
-    .my-item:hover{
-        -webkit-box-shadow: 11px 10px 19px -14px rgba(0,0,0,0.75);
-        -moz-box-shadow: 11px 10px 19px -14px rgba(0,0,0,0.75);
-        box-shadow: 11px 10px 19px -14px rgba(0,0,0,0.75);
-    }
-    .food-name{
-        font-weight: bolder;
-        font-size: 20px;
-        color: rgb(48, 47, 47);
-    }
-    .food-price{
-        font-size: 20px;
-        color: rgb(58, 58, 56);
-    }
+.texthead-in {
+  font-size: 7vh;
+  font-weight: bold;
+}
+.text-in {
+  font-size: 2vh;
+}
+.cardimg {
+  background-color: white;
+}
+.imgbg {
+  min-height: 50vh;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  width: 100%;
+}
+
+.food-name {
+  font-weight: bolder;
+  font-size: 20px;
+  color: rgb(48, 47, 47);
+}
+.food-price {
+  font-size: 20px;
+  color: rgb(58, 58, 56);
+}
+.moutain-in {
+  position: absolute;
+  z-index: 2;
+  top: 0%;
+  left: 0%;
+  color: white;
+}
+.button-order {
+  font-size: 2vh;
+  background: none;
+  cursor: pointer;
+  border: 0.15vw solid white;
+  color: white;
+  border-radius: 0.2vh;
+  padding: 0.5vh 1.2vh 0.5vh 1.2vh;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
-        <resheader></resheader>
-        <b-container class="mb-5">
-            <h1 class="mt-3">Shop Page</h1>
-            <div class="row">
-                <div v-for="product in products"  :key="product.id" class="col-12 col-lg-6 col-xl-4 my-2">
-                  <food :id="product.id" :name="product.name" :price="product.price" :image="product.image"></food>
+        <div class="img-top">
+            <resheader></resheader>
+            <h1 class="top-text">ARE YOU HUNGRY ? don't wait let start to order food now</h1>
+        </div>
+        <div class="mb-5">
+            <h1 class="mt-3">Choose & enjoy</h1>
+            <div class="container-fluid">
+                <div class="row">
+                        <div  v-for="product in products"  :key="product.id" class="col-12 col-lg-6 px-0 py-lg-2">
+                            <div v-if="product.id%2 !== 0">
+                                <food  class="pr-0 pr-lg-2" :id="product.id" :name="product.name" :price="product.price" :image="product.image"></food>
+                            </div>
+                            <div v-if="product.id%2 === 0">
+                                <food  class="pl-0 pl-lg-2 " :id="product.id" :name="product.name" :price="product.price" :image="product.image"></food>
+                            </div>
+                        </div> 
+                         
                 </div>
             </div>
-        </b-container>
+            
+        </div>
     </div>
 </template>
 <script>
@@ -16,53 +28,48 @@ import Food from '~/components/Food.vue'
 import Resheader from '~/components/Resheader.vue'
 
 export default {
-  components: { Resheader, Food },
+  components: { Resheader, Food  },
   
   data(){
       return{
           products: [
               {
                   id: 1,
-                  name: 'pizza',
-                  price: 300,
-                  image: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                  name: 'Carbonara Pasta',
+                  price: 10,
+                  image: 'https://images.unsplash.com/photo-1612874742237-6526221588e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80' 
               },
               {
                   id: 2,
-                  name: 'pancake',
+                  name: 'salad',
                   price: 100,
-                  image: 'https://images.unsplash.com/photo-1471478108131-9b2335c21611?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                  image: 'https://images.unsplash.com/photo-1529059997568-3d847b1154f0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
               },
               {
                   id: 3,
                   name: 'friench fries',
                   price: 50,
-                  image: 'https://images.unsplash.com/photo-1552895638-f7fe08d2f7d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80'
+                  image: 'https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
               },
               {
                   id: 4,
                   name: 'steak',
                   price: 500,
-                  image: 'https://images.unsplash.com/photo-1542365887-1149961dccc7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                  image: 'https://images.unsplash.com/photo-1585703900468-13c7a978ad86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80'
               },
               {
                   id: 5,
                   name: 'ice cream cone',
                   price: 100,
-                  image: 'https://images.unsplash.com/photo-1506444187582-b55b5c08d677?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                  image: 'https://images.unsplash.com/photo-1580559489333-755f7e3c8949?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
               },
               {
                   id: 6,
                   name: 'hamburger',
                   price: 150,
                   image: 'https://images.unsplash.com/photo-1556710807-aa697ab9e415?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-              },
-              {
-                  id: 7,
-                  name: 'banana',
-                  price: 10,
-                  image: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1409&q=80'
-              },
+              }
+              
 
           ]
       }
@@ -71,5 +78,26 @@ export default {
 }
 </script>
 <style scoped>
+
+.img-top {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.500)),
+    url(https://images.unsplash.com/photo-1553527922-767df645c5f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80);
+  min-height: 75vh;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  width: 100%;
+}
+.top-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  padding: 20px 15px;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  font-size: 10vh;
+}
 
 </style>
